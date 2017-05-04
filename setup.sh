810994987f44236605cfa366b19a2766b76b55ea
Paul Yiend <yiendos@sbmatrix.com>
2017-05-04 11:51:20 +0100

    Update to setup.sh
    I think with the advent of two factor authentication, simple calls to https://github.com/ no longer work. The user is prompted for username and password but authentication never works. 
    
    By calling `git@github.com:joomlatools/docman.git` directly ssh keys on the vagrant box are used instead

--- setup.sh
+++ setup.sh
@@ -5,20 +5,20 @@ git config --global credential.helper 'cache --timeout=3600'
 composer global update joomlatools/console
 joomla plugin:install joomlatools/console-joomlatools:dev-master
 
-[ ! -d ~/Projects/joomlatools-framework ] && git clone -b master https://github.com/joomlatools/joomlatools-framework.git ~/Projects/joomlatools-framework
+[ ! -d ~/Projects/joomlatools-framework ] && git clone -b master git@github.com:joomlatools/joomlatools-framework.git ~/Projects/joomlatools-framework
 
-[ ! -d ~/Projects/joomlatools-framework-files ] && git clone -b master https://github.com/joomlatools/joomlatools-framework-files.git ~/Projects/joomlatools-framework-files
-[ ! -d ~/Projects/joomlatools-framework-activities ] && git clone -b master https://github.com/joomlatools/joomlatools-framework-activities.git ~/Projects/joomlatools-framework-activities
-[ ! -d ~/Projects/joomlatools-framework-scheduler ] && git clone -b master https://github.com/joomlatools/joomlatools-framework-scheduler.git ~/Projects/joomlatools-framework-scheduler
-[ ! -d ~/Projects/joomlatools-framework-migrator ] && git clone -b master https://github.com/joomlatools/joomlatools-framework-migrator.git ~/Projects/joomlatools-framework-migrator
-[ ! -d ~/Projects/joomlatools-framework-ckeditor ] && git clone -b master https://github.com/joomlatools/joomlatools-framework-ckeditor.git ~/Projects/joomlatools-framework-ckeditor
-[ ! -d ~/Projects/joomlatools-framework-tags ] && git clone -b master https://github.com/joomlatools/joomlatools-framework-tags.git ~/Projects/joomlatools-framework-tags
+[ ! -d ~/Projects/joomlatools-framework-files ] && git clone -b master git@github.com:joomlatools/joomlatools-framework-files.git ~/Projects/joomlatools-framework-files
+[ ! -d ~/Projects/joomlatools-framework-activities ] && git clone -b master git@github.com:joomlatools/joomlatools-framework-activities.git ~/Projects/joomlatools-framework-activities
+[ ! -d ~/Projects/joomlatools-framework-scheduler ] && git clone -b master git@github.com:joomlatools/joomlatools-framework-scheduler.git ~/Projects/joomlatools-framework-scheduler
+[ ! -d ~/Projects/joomlatools-framework-migrator ] && git clone -b master git@github.com:joomlatools/joomlatools-framework-migrator.git ~/Projects/joomlatools-framework-migrator
+[ ! -d ~/Projects/joomlatools-framework-ckeditor ] && git clone -b master git@github.com:joomlatools/joomlatools-framework-ckeditor.git ~/Projects/joomlatools-framework-ckeditor
+[ ! -d ~/Projects/joomlatools-framework-tags ] && git clone -b master git@github.com:joomlatools/joomlatools-framework-tags.git ~/Projects/joomlatools-framework-tags
 
-[ ! -d ~/Projects/docman ] && git clone -b master https://github.com/joomlatools/docman.git ~/Projects/docman
-[ ! -d ~/Projects/fileman ] && git clone -b master https://github.com/joomlatools/fileman.git ~/Projects/fileman
-[ ! -d ~/Projects/logman ] && git clone -b master https://github.com/joomlatools/logman.git ~/Projects/logman
-[ ! -d ~/Projects/textman ] && git clone -b master https://github.com/joomlatools/textman.git ~/Projects/textman
-[ ! -d ~/Projects/connect ] && git clone -b master https://github.com/joomlatools/connect.git ~/Projects/connect
+[ ! -d ~/Projects/docman ] && git clone -b master git@github.com:joomlatools/docman.git ~/Projects/docman
+[ ! -d ~/Projects/fileman ] && git clone -b master git@github.com:joomlatools/fileman.git ~/Projects/fileman
+[ ! -d ~/Projects/logman ] && git clone -b master git@github.com:joomlatools/logman.git ~/Projects/logman
+[ ! -d ~/Projects/textman ] && git clone -b master git@github.com:joomlatools/textman.git ~/Projects/textman
+[ ! -d ~/Projects/connect ] && git clone -b master git@github.com:joomlatools/connect.git ~/Projects/connect
 
 if ! joomla plugin:list | grep -cq console-joomlatools; then 
   echo "ERROR: console-joomlatools plugin is not installed!"; exit 1; 
